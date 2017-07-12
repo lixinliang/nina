f712c0d2a55a4e423af1df047ed2f50760b0a566
李昕亮 <lixinliang@yy.com>
2017-07-12 17:55:38 +0800

    update bin

--- bin/nina.js
+++ bin/nina.js
@@ -9,6 +9,7 @@ const program = require('commander');
 
 program
     .option('run', 'Run app')
+    .option('build', 'Build app')
     .parse(process.argv);
 
 let folder = '/Users/lixinliang/GitHub/nina/';
@@ -16,4 +17,10 @@ let folder = '/Users/lixinliang/GitHub/nina/';
 if (program.run) {
     shelljs.cd(folder);
     shelljs.exec('electron .');
+}
+
+if (program.build) {
+    let dest = '/Users/lixinliang/Desktop/';
+    shelljs.cd(folder);
+    shelljs.exec(`electron-packager . Nina --platform=darwin --arch=x64 --overwrite --out=${ dest } --icon=./icon/logo.png --ignore='(.DS_Store)' --electron-version 1.4.12 && open ${ dest }`);
 }
