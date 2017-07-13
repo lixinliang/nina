589fe61daecf5c5b8bd21f5134ae9426078bb08b
李昕亮 <lixinliang@yy.com>
2017-07-13 17:23:43 +0800

    add page

---
+++ view/src/js/app/home/index.vue
@@ -0,0 +1,35 @@
+<script>
+// const ipc = require('ipc');
+
+export default {
+    data () {
+        return {
+            test : process.env == 'dev',
+        };
+    },
+    methods : {
+        click () {
+            // ipc.send('test', 'test');
+        },
+    },
+};
+</script>
+
+<template>
+    <div class="home" @click="click">home {{ test }}</div>
+</template>
+
+<style lang="scss" scoped>
+    .home {
+        width: 100%;
+        height: 100%;
+        padding-top: 25px;
+        overflow: hidden;
+        position: relative;
+        text-align: center;
+        font-size: 20px;
+        line-height: 1.5;
+        word-wrap: break-word;
+        word-break: break-all;
+    }
+</style>
