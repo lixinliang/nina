821ad2336fb0b9d8ab2192685c8070e43772819a
李昕亮 <lixinliang@yy.com>
2017-07-14 09:20:09 +0800

    add module

---
+++ module/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const createTray = require('./tray.js');
+const createMenu = require('./menu.js');
+const createIPC = require('./ipc.js');
+
+module.exports = {
+    createTray,
+    createMenu,
+    createIPC,
+};
