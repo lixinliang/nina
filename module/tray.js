88fab9c8116a82cf15e5af1339ce1fc6c9e06bf0
李昕亮 <lixinliang@yy.com>
2017-07-15 16:13:22 +0800

    tray

--- module/tray.js
+++ module/tray.js
@@ -1,21 +1,31 @@
 'use strict';
 
-const { Tray } = require('electron');
-const { name, logo } = require('./variable');
+const path = require('path');
+const { name } = require('./variable');
+const { Tray, nativeImage } = require('electron');
+const logo = nativeImage.createFromPath(path.join(__dirname, '../icon/logo-64x64.png'));
 
 let tray;
 
-module.exports = function ( win ) {
+module.exports = function ( win, ipc, sender ) {
 
     tray = new Tray(logo.resize({ width : 16, height : 16 }));
     tray.setToolTip(name);
     tray.on('click', () => {
         win.show();
     });
-    tray.on('drop-files', ( event, files ) => {
-        win.show();
-        event.preventDefault();
-        console.log(event, files);
+
+    ipc.async('drop-files', ( event ) => {
+        let { send } = sender(event);
+        tray.on('drop-files', ( event, files ) => {
+            win.show();
+            event.preventDefault();
+            send('drop-files', {
+                data : {
+                    files,
+                },
+            });
+        });
     });
 
 };
