cea9652eac8ab8ef8a4941c6a9d396c940c65351
李昕亮 <lixinliang@yy.com>
2017-07-15 16:13:38 +0800

    variable

--- module/variable.js
+++ module/variable.js
@@ -1,14 +1,12 @@
 'use strict';
 
 const path = require('path');
-const { app, nativeImage } = require('electron');
+const { app } = require('electron');
 
 const name = `${ app.getName().slice(0, 1).toUpperCase() }${ app.getName().slice(1).toLowerCase() }`;
 const version = app.getVersion();
-const logo = nativeImage.createFromPath(path.join(__dirname, '../icon/logo.png'));
 
 module.exports = {
     name,
     version,
-    logo,
 };
