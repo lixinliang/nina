40f7b2e4e03addf8a25d719911d609e5fb008c20
李昕亮 <lixinliang@yy.com>
2017-07-16 16:37:43 +0800

    update

--- bin/nina.js
+++ bin/nina.js
@@ -4,6 +4,7 @@
 
 const fs = require('fs');
 const path = require('path');
+const fse = require('fs-extra');
 const shelljs = require('shelljs');
 const program = require('commander');
 const packager = require('/usr/local/lib/node_modules/electron-packager');
@@ -12,11 +13,12 @@ program
     .option('run', 'Run app')
     .option('build', 'Build app')
     .option('debug', 'Debug app')
+    .option('test', 'Test generate')
     .parse(process.argv);
 
 function main () {
 
-    let folder = '/Users/lixinliang/GitHub/nina/';
+    let folder = path.join(__dirname, '..');
 
     if (program.run) {
         shelljs.cd(folder);
@@ -25,34 +27,34 @@ function main () {
     }
 
     if (program.build) {
-        let dest = '/Users/lixinliang/Desktop/';
-        packager({
-            'arch' : 'x64',
-            'platform' : 'darwin',
-            'dir' : `${ folder }`,
-            'out' : `${ dest }`,
-            'icon' : `${ path.join(folder, './icon/logo.icns') }`,
-            'ignore' : ['.DS_Store'],
-            'overwrite' : true,
-            'electronVersion' : '1.4.12',
-            'appCopyright' : 'Copyright (c) 2017 lixinliang',
-            'version-string':{
-                'ProductName' : 'Nina',
-                'InternalName' : 'Nina',
-                'OriginalFilename' : 'Nina',
-                'CompanyName' : 'lixinliang',
-                'FileDescription' : 'Nina Tool Desktop',
-            },
-        }, ( err ) => {
-            if (err) {
-                console.log(err);
-                return;
-            }
-            shelljs.exec(`open ${ dest }`);
+        let dest = path.join(folder, 'dest');
+        fse.ensureDir(dest).then(() => {
+            packager({
+                'arch' : 'x64',
+                'platform' : 'darwin',
+                'dir' : `${ folder }`,
+                'out' : `${ dest }`,
+                'icon' : `${ path.join(folder, './icon/logo.icns') }`,
+                'ignore' : ['.DS_Store'],
+                'overwrite' : true,
+                'electronVersion' : '1.4.12',
+                'appCopyright' : 'Copyright (c) 2017 lixinliang',
+                'version-string':{
+                    'ProductName' : 'Nina',
+                    'InternalName' : 'Nina',
+                    'OriginalFilename' : 'Nina',
+                    'CompanyName' : 'lixinliang',
+                    'FileDescription' : 'Nina Tool Desktop',
+                },
+            }, ( err ) => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                shelljs.exec(`open ${ dest }`);
+            });
         });
         return;
-        // shelljs.cd(folder);
-        // shelljs.exec(`electron-packager . Nina --platform=darwin --arch=x64 --overwrite --out=${ dest } --icon=./icon/logo.png --ignore='(.DS_Store)' --electron-version 1.4.12 && open ${ dest }`);
     }
 
     if (program.debug) {
@@ -60,6 +62,30 @@ function main () {
         shelljs.exec(`osascript -e 'tell application "Terminal" to do script "node-inspector"'`);
         shelljs.exec(`open -a "Google Chrome" "http://127.0.0.1:8080/debug?ws=127.0.0.1:8080&port=5858"`);
         shelljs.exec('electron --debug-brk=5858 . --env=debug');
+        return;
+    }
+
+    if (program.test) {
+        // const generate = require('../module/generate.js');
+        // let type = program.args[0];
+        // if (type == 'excel') {
+        //     generate.test.excel.test(path.join(folder, 'test/test-excel.xlsx'));
+        //     return;
+        // }
+        // if (type == 'ppt') {
+        //     generate.test.ppt.test(
+        //         path.join(folder, 'test/test-ppt.pptx'),
+        //         path.join(folder, 'test/output')
+        //     );
+        //     return;
+        // }
+        // generate([
+        //     path.join(folder, 'test/test.xlsx'),
+        //     path.join(folder, 'test/test.pptx'),
+        //     path.join(folder, 'test/output'),
+        // ], ({ err }) => {
+        //     err && console.log(err);
+        // });
         return;
     }
 
