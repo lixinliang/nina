955f8ef997f8867563459f3e7a204a69c9594b3f
李昕亮 <lixinliang@yy.com>
2017-07-16 16:37:33 +0800

    update

--- module/variable.js
+++ module/variable.js
@@ -1,10 +1,26 @@
 'use strict';
 
+const fs = require('fs');
 const path = require('path');
 const { app } = require('electron');
 
 const name = `${ app.getName().slice(0, 1).toUpperCase() }${ app.getName().slice(1).toLowerCase() }`;
 const version = app.getVersion();
+
+let copyright = '';
+
+try {
+    copyright = fs.readFileSync(path.join(__dirname, '../LICENSE'), 'utf8').split('\n')[2];
+} catch (e) {
+
+}
+
+app.setAboutPanelOptions({
+    version : '',
+    copyright,
+    applicationName : name,
+    applicationVersion : version,
+});
 
 module.exports = {
     name,
