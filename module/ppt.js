d71419844bb45336346b0af0b899a0e594f8525d
李昕亮 <lixinliang@yy.com>
2017-07-17 09:58:51 +0800

    update

--- module/ppt.js
+++ module/ppt.js
@@ -1,5 +1,50 @@
 'use strict';
 
-module.exports = function () {}
+const path = require('path');
+const PPT = require('ppt-template');
+const { Presentation, Slide } = PPT;
 
-// TODO: ppt
+module.exports = function ( excel, filename, output ) {
+    return new Promise(( resolve, reject ) => {
+        let ppt = new Presentation();
+        ppt.loadFile(filename).then(() => {
+            let length = ppt.getSlideCount();
+            let slides = Array.apply(null, { length }).map(( value, index ) =>
+                ppt.getSlide(index + 1).clone()
+            );
+
+            slides.forEach(( slide ) => {
+                slide.fillAll(excel);
+            });
+
+            ppt.generate(slides).then(( clone ) => {
+                clone
+                .saveAs(path.join(output, 'output.pptx'))
+                .then(resolve)
+                .catch(reject);
+            }).catch(reject);
+        }).catch(reject);
+    });
+};
+
+module.exports.test = function ( filename, output ) {
+    let ppt = new Presentation();
+    ppt.loadFile(filename).then(() => {
+        console.log('ppt read file success');
+        let length = ppt.getSlideCount();
+        console.log(`ppt slide length: ${ length }`);
+        let slides = Array.apply(null, { length }).map(( value, index ) =>
+            ppt.getSlide(index + 1).clone()
+        );
+        slides[0].fillAll([
+			Slide.pair('[Title]', 'Hello PPT'),
+		]);
+        ppt.generate(slides).then(( clone ) => {
+            clone
+            .saveAs(path.join(output, 'output.pptx'))
+            .then(() => {
+                console.log('done');
+            }).catch(( err ) => console.log(err));
+        }).catch(( err ) => console.log(err));
+    }).catch(( err ) => console.log(err));
+};
