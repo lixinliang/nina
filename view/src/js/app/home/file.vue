b2b4a1de1303c5f4532c8ec81d136612d4038c6f
李昕亮 <lixinliang@yy.com>
2017-07-17 09:59:16 +0800

    update

---
+++ view/src/js/app/home/file.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    props : ['multiple'],
+    methods : {
+        select ({ target }) {
+            let { files } = target;
+            if (files.length) {
+                this.$emit('select', files[0].path);
+                return;
+            }
+            this.$emit('select', '');
+        },
+    },
+};
+</script>
+
+<template>
+    <input type="file"
+        :tabIndex="-1"
+        @change="select"
+        :multiple="multiple"
+        :directory="multiple"
+        :webkitdirectory="multiple"
+    >
+</template>
+
+<style lang="scss" scoped>
+    input {
+        position: absolute;
+        top: 100%;
+        opacity: 0;
+    }
+</style>
