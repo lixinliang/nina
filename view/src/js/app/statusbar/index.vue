6cfb8134366d65c284d0a79ab54ff0d6d3294ab3
李昕亮 <lixinliang@yy.com>
2017-07-18 09:42:31 +0800

    update

---
+++ view/src/js/app/statusbar/index.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+};
+</script>
+
+<template>
+    <div class="statusbar"></div>
+</template>
+
+<style lang="scss" scoped>
+    .statusbar {
+        position: fixed;
+        z-index: 1;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 22px;
+        background-color: rgba(0, 0, 0, .6);
+    }
+</style>
