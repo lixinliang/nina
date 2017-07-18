de68490f97797d598eb93d8b5b2d8aa73735aa5e
李昕亮 <lixinliang@yy.com>
2017-07-18 09:42:23 +0800

    update

---
+++ view/src/js/app/index.vue
@@ -0,0 +1,27 @@
+<script>
+import store from './store';
+import router from './router';
+import statusbar from './statusbar';
+
+export default {
+    store,
+    router,
+    components : {
+        statusbar,
+    },
+};
+</script>
+
+<template>
+    <div id="app" ref="app">
+        <statusbar></statusbar>
+        <router-view ref="router-view"></router-view>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    #app {
+        width: 100%;
+        height: 100%;
+    }
+</style>
